25965d25e5d271524950fec125b78324dd910674
Oscar Anillo <oscarani14@gmail.com>
2023-06-02 13:27:09 -0500

    add components without content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+/* Components */
 import { NavComponent } from './Components/Nav-component'
+import { HomeComponent } from './Components/HomeComponent'
+import { Products } from './Components/Products'
+import { Contact } from './Components/Contact'
+import { Login } from './Components/Login'
+
+/* React-router-dom */
+import { Route, Routes } from 'react-router-dom'
 
 import './App.css'
 
@@ -9,6 +17,12 @@ function App() {
     <div className='App'>
       <div className='container'>
         <NavComponent />
+        <Routes>
+          <Route path='/' element={<HomeComponent />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </div>
     </div>
   )
--- src/Components/Nav-component.jsx
+++ src/Components/Nav-component.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { Link } from 'react-router-dom';
 
 export const NavComponent = () => {
     const [openMobileMenu, setOpenMobileMenu] = useState(false);
@@ -7,15 +8,16 @@ export const NavComponent = () => {
     const clickHandler = () => {
         setOpenMobileMenu(!openMobileMenu)
     }
+
     return (
         <div className="nav-div">
             <h1>Best Wine</h1>
             <nav className="nav-desktop">
                 <ul>
-                    <li>Home</li>
-                    <li>Products</li>
-                    <li>Contact</li>
-                    <li>Login</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/login">Login</Link></li>
                 </ul>
             </nav>
             <GiHamburgerMenu onClick={clickHandler}/>  
@@ -25,10 +27,10 @@ export const NavComponent = () => {
                         <div className='div-mobile-menu-inner'>
                             <button onClick={clickHandler}>X</button>
                             <ul>
-                                <li>Home</li>
-                                <li>Products</li>
-                                <li>Contact</li>
-                                <li>Login</li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/products">Products</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                                <li><Link to="/login">Login</Link></li>
                             </ul>
                         </div>
                     </div>
